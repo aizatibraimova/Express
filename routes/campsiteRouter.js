0f0104d79d6c424eff247a39aa79a476c80d2af3
aizatibraimova <97326728+aizatibraimova@users.noreply.github.com>
2023-08-12 11:23:52 -0500

    Week 1, Task 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,25 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.params.campsiteId}
+    with description: ${req.params.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on ${req.params.campsiteId} with description: ${req.params.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`)
+});
+
 module.exports = campsiteRouter;
